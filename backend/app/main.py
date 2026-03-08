fd490bbf11d692d2fa3081c394c1ab36f8843ed0
oliviesalat <semyon.suslov@gmail.com>
2026-03-08 19:02:11 +0100

    config for starting server

---
+++ backend/app/main.py
@@ -0,0 +1,48 @@
+from fastapi import FastAPI
+from .config import settings
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from .routes import products, categories, cart
+from .database import init_db
+from contextlib import asynccontextmanager
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    init_db()
+    print("Starting application...")
+    yield
+    print("Application stopped")
+
+app = FastAPI(
+    title=settings.app_name,
+    debug=settings.debug,
+    docs_url='/api/docs',
+    redoc_url='/api/redoc',
+    lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.cors_origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*']
+)
+
+app.mount('/static', StaticFiles(directory=settings.static_dir), name='static')
+app.include_router(products.router)
+app.include_router(categories.router)
+app.include_router(cart.router)
+
+@app.get('/')
+def root():
+    return {
+        'message': 'hello',
+        'docs': 'api/docs',
+    }
+
+@app.get('/healthcheck')
+def healthcheck():
+    return {'status': 'healthy'}
+
